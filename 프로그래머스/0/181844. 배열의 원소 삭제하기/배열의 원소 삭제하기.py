20dba4689b68009d14de8a1ea01517e46d12ecd3
guruggurug <jbean.phone@gmail.com>
2026-06-17 09:14:00 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,8 +1,12 @@
 def solution(arr, delete_list):
-    arr_org = {j: i for i, j in enumerate(arr)}
-    arr_del = set(arr) - set(delete_list)
-    answer = {}
-    for num in arr_del:
-        answer[num] = arr_org[num]
-    answer = sorted(answer, key = answer.get)
-    return answer
+    # arr_org = {j: i for i, j in enumerate(arr)}
+    # arr_del = set(arr) - set(delete_list)
+    # answer = {}
+    # for num in arr_del:
+    #     answer[num] = arr_org[num]
+    # answer = sorted(answer, key = answer.get)
+    # return answer
+    for i in delete_list:
+        if i in arr:
+            arr.remove(i)
+    return arr
