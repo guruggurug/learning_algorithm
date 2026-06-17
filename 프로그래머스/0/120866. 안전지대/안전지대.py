ba5be06b620b5e4a1974bc935123c91d3c925567
guruggurug <jbean.phone@gmail.com>
2026-06-17 14:50:20 +0900

    [level 0] Title: 안전지대, Time: 1.63 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,3 +1,4 @@
+# 나의 풀이 2
 def solution(board):
     #[[0, 0, 0, 0, 0],
     # [0, 0, 0, 0, 0],
@@ -6,35 +7,37 @@ def solution(board):
     # [0, 0, 0, 0, 0]]
     
     # 지뢰의 위치 [3][2]
-    # 상하 탐색 [2][2] [4][2] ---> [i][j]에서 [i-1] & [i+1]
-    # 좌우 탐색 [3][1] [3][3] ---> [i][j]에서 [j-1] & [j+1]
-    # 대각선 탐색 ---> 좌우상하 탐색의 조합
-    
-    # 아이디어 1: 원소 하나씩 살펴보며 좌우상하, 대각선에 1이 있는지 확인
+
     n = len(board)
     result = 0
+    bomb = []
+
+    # 지뢰 찾기
     for i in range(n):
         for j in range(n):
-            flag = 0
             if board[i][j] == 1:
-                print('지뢰', i, j)
-                continue
-            ind = {i:[], j:[]}
-            for x in [-1,+1]:
-                ind[i] += [i+x] if i+x >= 0 and i+x <= n-1 else []
-                ind[j] += [j+x] if j+x >= 0 and j+x <= n-1 else []
-            # {3: [2,4], 2: [1,3]}
-            
-            region = []
-            region += [(i,x) for x in ind[j]]
-            region += [(x,j) for x in ind[i]]
-            region += [(x,y) for x in ind[i] for y in ind[j]]
-            # region = [(3,1) (3,3), (2,2) (4,2), ...]
-            print(i,j, '살펴보는 중..', region)
-            for x, y in region:
-                if board[x][y] == 1:
-                    flag = 1
-                    print('삐빕', i, j)
-            if flag == 0:
-                result += 1 # result += 1은 언제 해야 하는가....
-    return result
+                bomb.append((i,j))
+                # bomb 예시 결과 [(3,2)]
+    
+    # 상하 탐색 [2][2] [4][2] ---> [i][j]에서 [i-1] & [i+1]
+    # 좌우 탐색 [3][1] [3][3] ---> [i][j]에서 [j-1] & [j+1]
+    # 대각선 탐색 ---> [i-1] & [i+1] / [j-1] & [j+1]의 조합
+
+    # 인접 지역 좌표 구하기
+    region = []
+    for x, y in bomb:
+        for m in [-1,+1]:
+            region += [(x, y+m), (x+m, y), (x+m, y+m), (x-m, y+m)]
+            print(region)
+            # region 예시 결과 [()]
+
+    # 인접 지역 데이터 1로 바꾸기
+    for x, y in region:
+        print(x,y)
+        if x in range(n) and y in range(n):
+            board[x][y] = 1
+    
+    print(board)
+
+    # 안전 지대 구하기
+    return n*n - sum([sum(i) for i in board])
