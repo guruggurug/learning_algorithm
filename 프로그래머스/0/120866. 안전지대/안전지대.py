682fecf85d13d8b8a503f50bc6cf90e794a0abc1
guruggurug <jbean.phone@gmail.com>
2026-06-17 15:47:56 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,13 +1,4 @@
-# 나의 풀이 2
 def solution(board):
-    #[[0, 0, 0, 0, 0],
-    # [0, 0, 0, 0, 0],
-    # [0, 0, 0, 0, 0],
-    # [0, 0, 1, 0, 0],
-    # [0, 0, 0, 0, 0]]
-    
-    # 지뢰의 위치 [3][2]
-
     n = len(board)
     result = 0
     bomb = []
@@ -17,27 +8,18 @@ def solution(board):
         for j in range(n):
             if board[i][j] == 1:
                 bomb.append((i,j))
-                # bomb 예시 결과 [(3,2)]
-    
-    # 상하 탐색 [2][2] [4][2] ---> [i][j]에서 [i-1] & [i+1]
-    # 좌우 탐색 [3][1] [3][3] ---> [i][j]에서 [j-1] & [j+1]
-    # 대각선 탐색 ---> [i-1] & [i+1] / [j-1] & [j+1]의 조합
 
-    # 인접 지역 좌표 구하기
-    region = []
-    for x, y in bomb:
-        for m in [-1,+1]:
-            region += [(x, y+m), (x+m, y), (x+m, y+m), (x-m, y+m)]
-            print(region)
-            # region 예시 결과 [()]
+    dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+    dy = [0, 0, -1, 1, -1, 1, -1, 1]
 
-    # 인접 지역 데이터 1로 바꾸기
-    for x, y in region:
-        print(x,y)
-        if x in range(n) and y in range(n):
-            board[x][y] = 1
-    
-    print(board)
+    for i, j in bomb:
+        for k in range(8):
+            nx = i + dx[k]
+            ny = j + dy[k]
+            
+            if 0 <= nx < n and 0 <= ny < n:
+                if board[nx][ny] == 0:
+                    board[nx][ny] = 1
+                pass
 
-    # 안전 지대 구하기
-    return n*n - sum([sum(i) for i in board])
+    return sum(row.count(0) for row in board)
