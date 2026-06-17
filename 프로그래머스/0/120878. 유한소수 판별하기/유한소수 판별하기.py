2b42e9b4bf28a0caee93b21b55b06a8356f54f9d
guruggurug <jbean.phone@gmail.com>
2026-06-17 11:32:30 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,9 +1,20 @@
-import math
+# import math
 
 def solution(a, b):
-    divisor = math.gcd(a,b)
-    rem = b // divisor
+    # divisor = math.gcd(a,b)
+    # rem = b // divisor
     
+    c = b
+    
+    while b != 0:
+        temp = a % b
+        
+        a = b
+        b = temp
+    
+    divisor = a
+    rem = c // divisor
+    # return divisor
     while rem % 2 == 0:
         rem /= 2
     
