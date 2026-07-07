1de60ef58e56ed5f831da4b56035de6ea7c59d98
guruggurug <jbean.phone@gmail.com>
2026-07-07 12:10:21 +0900

    [level 0] Title: A로 B 만들기, Time: 0.21 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,9 @@
+def solution(before, after):
+    before_list = list(before)
+    after_list = list(after)
+    before_list.sort()
+    after_list.sort()
+    if before_list == after_list:
+        return 1
+    else:
+        return 0
